9b6044c80250d4f22c636db7479a095d663aea45
Dino Trojak <dinotrojak@gmail.com>
2016-11-24 21:22:26 +0100

    added hacker news feed

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,60 @@
 import React, { Component } from 'react';
+import axios from "axios";
+
 import logo from '../img/rit.png';
 import '../styles/App.css';
 
-
 class App extends Component {
+  state = {
+    hackernews: []
+  }
+  constructor(props) {
+    super(props)
+    this.getHackerNewsData = this.getHackerNewsData.bind(this);
+    this.getSingleNews = this.getSingleNews.bind(this);
+  }
+  componentDidMount() {
+    this.getHackerNewsData();
+  }
+  getHackerNewsData() {
+    const self = this;
+    axios.get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+    .then(function (response) {
+      self.getSingleNews(response.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+  getSingleNews(news) {
+    const self = this;
+    news.forEach(n => {
+      axios.get(`https://hacker-news.firebaseio.com/v0/item/${n}.json?print=pretty`)
+      .then(function (response) {
+        const hackernews = self.state.hackernews;
+        hackernews.push(response.data);
+        self.setState({ hackernews });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    })
+  }
+  renderItem(news) {
+    return (<a key={news.id} href={news.url} className="list-group-item" target="_blank">
+      <h4 className="list-group-item-heading">{news.title}</h4>
+    </a>);
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Twitter feed </h2>
+          <h2>Hacker news feed</h2>
         </div>
         <div className="container">
           <div className="list-group">
-            <a href="#" className="list-group-item active">
-              <h4 className="list-group-item-heading">List group item heading</h4>
-              <p className="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </a>
+            {this.state.hackernews.map(this.renderItem)}
           </div>
         </div>
       </div>
