ec1c9559762af43026cdc99397b473db6ed824fd
Dino Trojak <dinotrojak@gmail.com>
2016-11-24 15:38:57 +0100

    added bootstrap and logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from '../img/logo.svg';
+import logo from '../img/rit.png';
 import '../styles/App.css';
+
 
 class App extends Component {
   render() {
@@ -8,11 +9,16 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Twitter feed </h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <div className="list-group">
+            <a href="#" className="list-group-item active">
+              <h4 className="list-group-item-heading">List group item heading</h4>
+              <p className="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
